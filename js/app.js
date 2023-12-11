7b5adb2fe50a58c19a5df957d96321a2668e05ef
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-12-12 03:58:04 +0600

    header done

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$('.hero-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
