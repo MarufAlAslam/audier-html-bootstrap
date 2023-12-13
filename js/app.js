1bbef867d20917583691d945476b7ded73c2e970
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-12-13 16:12:21 +0600

    effects added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
-$('.hero-slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    cssEase: 'linear',
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-    slidesToShow: 1,
-    slidesToScroll: 1
+AOS.init({
+  duration: 1200,
+});
+
+$(".hero-slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  cssEase: "linear",
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
+// make the nav sticky after scrolling 20 px
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 20) {
+    $("nav").addClass("sticky");
+  } else {
+    $("nav").removeClass("sticky");
+  }
 });
